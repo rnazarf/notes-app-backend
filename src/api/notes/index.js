202ca0724c71760e27642780c6e1b2a3cf411ed7
rnazarf <rifkinazar24@gmail.com>
2021-06-26 11:49:45 +0800

    restructure with hapi plugin

---
+++ src/api/notes/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+const NotesHandler = require('./handler');
+const routes = require('./routes');
+
+module.exports = {
+  name: 'notes',
+  version: '1.0.0',
+  register: async (server, { service }) => {
+    const notesHandler = new NotesHandler(service);
+    server.route(routes(notesHandler));
+  },
+};
