4e809c4bc3404eb10704c253b1b011479af357da
rnazarf <rifkinazar24@gmail.com>
2021-07-02 09:36:59 +0800

    update en

--- src/server.js
+++ src/server.js
@@ -9,7 +9,8 @@ const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
     port: process.env.PORT,
-    host: process.env.HOST,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    // host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
