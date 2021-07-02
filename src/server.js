b9c7087840a7773c16c66c95264d1d09038abf5f
rnazarf <rifkinazar24@gmail.com>
2021-07-02 09:50:13 +0800

    perbaikan

--- src/server.js
+++ src/server.js
@@ -9,8 +9,7 @@ const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
     port: process.env.PORT,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-    // host: process.env.HOST,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
